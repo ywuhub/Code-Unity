ad839263cd7c57b2cd835fd5948696a03b4c7127
Jackson Luu <jacksonluu2006@hotmail.com>
2019-10-20 01:18:34 +1100

    added html

--- server/routes.py
+++ server/routes.py
@@ -2,8 +2,15 @@
 This file provides routing for static and server generated files.
 """
 from server import app
+from flask import render_template, request
+from pymongo.database import Database
 
 
-@app.route("/", methods=["GET"])
+@app.route("/", methods=["GET", "POST"])
 def index():
-    return "index.html!"
+    return render_template('index.html')
+
+@app.route("/project", methods=["GET", "POST"])
+def new_project():
+    #db = Database.get_collection("projects")
+    return render_template('new_project.html')
