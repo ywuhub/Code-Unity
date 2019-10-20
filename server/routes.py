e460608cda04bf921364599b7abcf7c490fd0dec
Yuxing Wu <allan.wu6@gmail.com>
2019-10-20 15:06:41 +1100

    writing where to replace when implementing with front

--- server/routes.py
+++ server/routes.py
@@ -20,7 +20,8 @@ def new_project():
 def user_profile(user_id):
     if db['profiles'].count_documents({"_id": ObjectId(user_id)}, limit = 1) == 0: # check if user exists
         return render_template('404.html') # return 404 error if user does not exists
+                                           # WE HAVE TO PLACE THIS LATER (need to return error)
     else:
         data = db['profiles'].find_one({"_id": ObjectId(user_id)}) # get the user's profile information
-        print(data['name'])
-        return render_template('profile.html', profile = data)
+        return render_template('profile.html', profile = data) # WE HAVE TO PLACE THIS LATER (need to return json data)
+        
