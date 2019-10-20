1037edb2817827ae80cf984820fc7dbd69fc1315
Yuxing (Allan) Wu <36904598+ywuhub@users.noreply.github.com>
2019-10-20 15:10:39 +1100

    Profile Page Information: API Backend (#2)
    * init commit with profile page information api
    
    * completed the retrieval of all the profile information per user to front end
    
    * writing where to replace when implementing with front

--- server/routes.py
+++ server/routes.py
@@ -1,9 +1,10 @@
 """
 This file provides routing for static and server generated files.
 """
-from server import app
+from server import app, db
 from flask import render_template, request
 from pymongo.database import Database
+from bson.objectid import ObjectId
 
 
 @app.route("/", methods=["GET", "POST"])
@@ -14,3 +15,13 @@ def index():
 def new_project():
     #db = Database.get_collection("projects")
     return render_template('new_project.html')
+
+@app.route("/profile/<user_id>", methods=["GET"])
+def user_profile(user_id):
+    if db['profiles'].count_documents({"_id": ObjectId(user_id)}, limit = 1) == 0: # check if user exists
+        return render_template('404.html') # return 404 error if user does not exists
+                                           # WE HAVE TO PLACE THIS LATER (need to return error)
+    else:
+        data = db['profiles'].find_one({"_id": ObjectId(user_id)}) # get the user's profile information
+        return render_template('profile.html', profile = data) # WE HAVE TO PLACE THIS LATER (need to return json data)
+        
