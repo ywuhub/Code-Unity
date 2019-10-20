a6f15cba87cdb03bb0d63c9ce1dba11f34729ee3
Yuxing Wu <allan.wu6@gmail.com>
2019-10-20 13:21:44 +1100

    init commit with profile page information api

--- server/routes.py
+++ server/routes.py
@@ -1,7 +1,7 @@
 """
 This file provides routing for static and server generated files.
 """
-from server import app
+from server import app, db
 from flask import render_template, request
 from pymongo.database import Database
 
@@ -14,3 +14,10 @@ def index():
 def new_project():
     #db = Database.get_collection("projects")
     return render_template('new_project.html')
+
+@app.route("/profile/<user_id>", methods=["GET"])
+def user_profile(user_id):
+    if db['profiles'].count_documents({'_id': user_id}, limit = 1) == 0:
+        return render_template('404.html')
+    else:
+        return render_template('profile.html')
