9ae18db014891de740e7517b705a1f80b9bc386a
Zhiwei Cao <zhiwei.cao@student.unsw.edu.au>
2019-10-31 18:42:44 +1100

    profile edit tag with issue

--- frontend/code-unity/src/Board/Profile.jsx
+++ frontend/code-unity/src/Board/Profile.jsx
@@ -1,10 +1,13 @@
 import React from 'react';
 
 import { userService } from '@/_services';
+import '@/Style';
 
 class Profile extends React.Component {
     constructor(props) {
         super(props);
+        this.removeTag = this.removeTag.bind(this);
+        this.addTag = this.addTag.bind(this);
         this.state = {
             "_id": "",
             name: "",
@@ -38,9 +41,49 @@ class Profile extends React.Component {
      // <div class="d-flex justify-content-between flex-wrap flex-md-nowrap align-items-center pt-3 pb-2 mb-3 border-bottom">
      //                <button type="button" class="btn btn-sm btn-outline-secondary" data-target="#edit" data-toggle="tab">Edit Profile</button>
      //                </div>
+    removeTag(e) {
+        e.preventDefault();
+        if (e.target.classList.contains('interests')) {
+            alert('Enter interests!!!');    
+          let tags = this.state.interests;
+          const tagIndex = tags.indexOf(e.target.value);
+          if (tagIndex !== -1) {
+            tags.splice(tagIndex, 1);
+            this.setState({ interests: tags });
+        }
 
+        } else if (e.target.classList.contains('programming_languages')) {
+            alert('Enter programming_languages!!!');    
+          let tags = this.state.programming_languages;
+          const tagIndex = tags.indexOf(e.target.value);
+          if (tagIndex !== -1) {
+            tags.splice(tagIndex, 1);
+            this.setState({ programming_languages: tags });
+            }
+        }
+    }
 
+    addTag(e) {
+        if (e.key === 'Enter') {
+            alert('Enter clicked!!!');    
+            // if (e.target.className.indexOf('interests') !== -1) {
+            //   // let tags = this.state.interests;
+            //   // if (tagIndex !== -1) {
+            //   //   tags.splice(tagIndex, 1);
+            //   //   this.setState({ interests: tags });
+            //   //   // this.filterByTag(true);
+
+            // } else if (e.target.className.indexOf('programming_languages') !== -1) {
+              let tags = this.state.programming_languages;
+              const newTag = e.target.value;
+              tags.push(newTag);
+              this.setState({ programming_languages: tags });
+                // this.filterByTag(true);
+            // }
+        }
+    }
     render() {
+        let tag_id = 0;
         return (
             <div class="container">
                 <div class="d-flex justify-content-between flex-wrap flex-md-nowrap align-items-center pt-3 pb-2 mb-3 border-bottom">
@@ -90,9 +133,7 @@ class Profile extends React.Component {
                                                     {
                                                         this.state.interests.map((item) => {
                                                             return(
-                                                                <li>
-                                                                    {item}
-                                                                </li>
+                                                                    <a href="#" class="badge badge-dark badge-pill mr-2">{item}</a>
                                                                 )
                                                         })                            
                                                     }
@@ -138,18 +179,62 @@ class Profile extends React.Component {
                                             <input class="form-control" type="text" defaultValue={this.state.github} />
                                         </div>
                                     </div>
-
                                     <div class="form-group row">
                                         <label class="col-lg-3 col-form-label form-control-label">Interests</label>
                                         <div class="col-lg-9">
-                                            <input class="form-control" type="text" defaultValue="" />
+                                            <div class="profile-descrption-block">
+                                                {
+                                                    this.state.interests.map((item) => {
+                                                        return( 
+                                                                <span className="badge badge-pill badge-success mr-2" key={item + ' ' + tag_id++}>
+                                                                {item}
+                                                                <button className="fa fa-times bg-transparent border-0 p-0 pl-1 interests" 
+                                                                        value={item} 
+                                                                        style={{ 'outline': 'none' }} 
+                                                                        onClick={this.removeTag.bind(this)}>
+                                                                </button>
+                                                                </span>
+
+                                                            )
+                                                    })                            
+                                                }
+                                            </div>
                                         </div>
                                     </div>
                                     <div class="form-group row">
                                         <label class="col-lg-3 col-form-label form-control-label">Programming language</label>
                                         <div class="col-lg-9">
-                                            <input class="form-control" type="text" defaultValue="" />
+                                            <div class="profile-descrption-block">
+                                                <span class="d-flex flex-wrap">
+                                                {
+                                                    this.state.programming_languages.map((item) => {
+                                                        return(
+                                                                <span className="badge badge-pill badge-success mr-2" key={item + ' ' + tag_id++}>
+                                                                    {item}
+                                                                    <button className="fa fa-times bg-transparent border-0 p-0 pl-1 programming_languages" 
+                                                                            value={item} 
+                                                                            style={{ 'outline': 'none' }} 
+                                                                            onClick={this.removeTag.bind(this)}>
+                                                                    </button>
+                                                                </span>
+                                                                )
+                                                    })                            
+                                                }
+                                                <span className="badge badge-pill badge-primary mr-2" >
+                                                        <input type="text" 
+                                                        placeholder="add new tag" 
+                                                        className="profile-tag-input" 
+                                                        onKeyPress={this.addTag.bind(this)}/>
+                                                </span>
+                                                <button className="fa fa-times bg-transparent border-0 p-0 pl-1 programming_languages" 
+                                                        value="???" 
+                                                        style={{ 'outline': 'none' }} 
+                                                        onKeyPress={this.addTag.bind(this)}>
+                                                </button>
+                                                </span>
+                                            </div>
                                         </div>
+
                                     </div>
                                     <div class="form-group row">
                                         <label class="col-lg-3 col-form-label form-control-label">About me</label>
