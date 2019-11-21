7829e14a35aa19b7695fb1a8527bc82c677fef39
Jason Do <Jason.Do1@hotmail.com>
2019-11-21 21:57:09 +1100

    fixed multiple parameters fetching in advanced search, searching current user directs to profile, show avatar for other users profile

--- frontend/code-unity/src/UserSearch/OthersProfile.jsx
+++ frontend/code-unity/src/UserSearch/OthersProfile.jsx
@@ -13,14 +13,21 @@ class OthersProfile extends React.Component {
             myProjects: [],
             not_found: false,
             isLoading: false,
-            avatar: ''
+            avatar: '',
+            alert: ''
         }
     }
 
     componentDidMount() {
         this.setState({ isLoading: true });
         const { _id } = this.props.location.state;
-        userService.getUserAccountDetails().then(data => { this.setState({ avatar: data.avatar })});
+        const options = { method: 'GET', headers: { 'Content-Type': 'application/json', 'Authorization': authHeader() } };
+        fetch(`${config.apiUrl}/api/user_list?user_ids=${_id}`, options)
+            .then(response => { return response.json(); })
+            .then(users => {
+                if (users.length > 0) this.setState({ avatar: users[0].avatar });
+            })
+            .catch(err => { console.log(err); });
         userService.getUserProfile(_id).then(data => {
             this.setState({
                 id_: _id,
@@ -44,18 +51,20 @@ class OthersProfile extends React.Component {
 
     inviteUser(e) {
         const group = document.getElementById('group-name').value;
-        
+
         const options = {
             method: 'POST',
             headers: { 'Content-Type': 'application/json', 'Authorization': authHeader() },
             body: JSON.stringify({ project_id: group })
         };
-    
+
         fetch(`${config.apiUrl}/api/user/${this.state.id_}/invite`, options)
             .then(response => { return response.json(); })
             .then(json => {
                 if (json.message) {
-                    alert(json.message);
+                    this.setState({ alert: json.message });
+                } else {
+                    this.setState({ alert: json.status });
                 }
             })
             .catch(err => { console.log(err); })
@@ -64,6 +73,10 @@ class OthersProfile extends React.Component {
     render() {
         return (
             <div className="container-fluid">
+                {this.state.alert !== '' &&
+                    <Foo msg={this.state.alert} />
+                }
+
                 {(this.state.isLoading && <div className="d-flex spinner-border text-dark mx-auto mt-5 p-3"></div>) ||
                     <div>
                         <div className="d-flex justify-content-between flex-wrap flex-md-nowrap align-items-center pt-3 pb-2 mb-3 border-bottom">
@@ -93,7 +106,7 @@ class OthersProfile extends React.Component {
                                         <div className="m-4">
                                             <div className="row">
                                                 <div className="col-md-6 avator-container">
-                                                    <img src={this.state.avatar} className="mx-auto img-fluid img-circle d-block rounded-circle" alt="avatar" />    
+                                                    <img src={this.state.avatar} className="mx-auto img-fluid img-circle d-block rounded-circle" alt="avatar" />
                                                 </div>
                                                 <div className="col-md-6 align-middle">
                                                     <div>
@@ -146,7 +159,7 @@ class OthersProfile extends React.Component {
                             <div className="modal-body">
                                 <form>
                                     <div className="error">
-                                        
+
                                     </div>
                                     <label htmlFor="group-name" className="col-form-label">Group Name:</label>
                                     <select className="form-control mb-3" id="group-name">
@@ -168,6 +181,14 @@ class OthersProfile extends React.Component {
             </div>
         )
     }
+}
+
+function Foo(props) {
+    return (
+        <div className="alert alert-info mt-2">
+            <strong>Notice!</strong> {props.msg}
+        </div>
+    )
 }
 
 export { OthersProfile };
