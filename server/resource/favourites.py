5a65063f3e8c5098a71488d33fde966bcd778d17
Allan <36904598+ywuhub@users.noreply.github.com>
2019-11-21 11:03:35 +1100

    POST request favourites api endpoint done
    adds project to favourites list

--- server/resource/favourites.py
+++ server/resource/favourites.py
@@ -13,7 +13,7 @@ class FavouriteProjects(Resource):
     def get(self):
         """
         Fetch the list of favourited projects for the currently logged in user
-        Input:
+        Input (url parameters):
         - user_id: string
           description: the user id from the parameters to fetch their favourite projects
 
@@ -46,7 +46,7 @@ class FavouriteProjects(Resource):
         args = get_parser.parse_args(strict=True)
 
         # get user_id
-        user_id = args["userid"]
+        user_id = args["user_id"]
 
         # convert user_id (string) into ObjectId
         try:
@@ -69,14 +69,83 @@ class FavouriteProjects(Resource):
                     return user_favourites, 200    
         else:
             return {"message": "user id required to fetch the favourites list"}, 400
-    """
-    Add the selected project to the user's favourites list
-    """    
-    def post(self):
-        pass
     
-    """
-    Remove the selected project from the user's favourites list
-    """
+    def post(self):
+        """
+        Add the selected project to the user's favourites list
+        Input (url parameters):
+        - user_id: string
+        description: the user id for the project to be added to their favourites
+        - project_id: string
+        description: the project id to be added to the user's favourites
+        
+        EXAMPLE:
+        POST api/favourites ->
+            (200 OK) <-
+                {
+                    "message": "project added to the user's favourites"
+                }
+        """
+        # fetch parameter
+        get_parser = reqparse.RequestParser(bundle_errors=True)
+        get_parser.add_argument("user_id", required=True, help="User ID required to ad to their favourite projects")
+        get_parser.add_argument("project_id", required=True, help="Project ID required to add to the favourite projects")
+        args = get_parser.parse_args(strict=True)
+
+        # get user_id and project_id
+        user_id = args["user_id"]
+        project_id = args["project_id"]
+
+        # convert parameter ids into objectids
+        try:
+            user_id = ObjectId(user_id)
+            project_id = ObjectId(project_id)
+        except:
+            return {"message": "invalid user id or project id"}, 400
+
+        # add project to the user's favourites
+        if 'user_id' or 'project_id' not in args:
+            return {"message": "both user and project id are required"}, 400
+        else:
+            # check if user is valid
+            user = self.users.find_one({"_id": user_id})
+            project = self.projects.find_one({"_id": project_id})
+            if user is None:
+                return {"message": "user not found"}, 404
+            elif project is None:
+                return {"message": "project not found"}, 404
+            else:
+                # add project to favourites
+                user_favourites = self.favourites.find_one({"user_id": user_id})
+                if user_favourites is None:
+                    # insert a new doc into favourites collection
+                    favourites_list = []
+                    self.favourites.insert({
+                        "user_id": user_id,
+                        "favourite_projects": favourites_list.append(project)
+                    })
+                else:
+                    new_favourite_list = user_favourites["favourite_projects"].append(project)
+                    updated_list = {"favourite_projects": new_favourite_list}
+                    self.favourites.update({"user_id": user_id}, {"set": updated_list}, upsert=False)
+            
+            return {"status": "project has be added to favourites successfully"}, 200
+    
     def put(self):
+        """
+        Remove the selected project from the user's favourites list
+        Input (url parameters):
+            - user_id: string
+            description: the user id for the project to be removed to their favourites
+            - project_id: string
+            description: the project id to be removed to the user's favourites
+            
+            EXAMPLE:
+            PUT api/favourites ->
+                (200 OK) <-
+                    {
+                        "message": "project removed from the user's favourites"
+                    }
+        """
+        
         pass
