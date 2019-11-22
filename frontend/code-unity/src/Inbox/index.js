134ec869cac276bf88590964ac18bf4ca6e14a5a
Jason Do <Jason.Do1@hotmail.com>
2019-11-22 22:03:17 +1100

    show notifications in dashboard

--- frontend/code-unity/src/Inbox/index.js
+++ frontend/code-unity/src/Inbox/index.js
@@ -2,3 +2,4 @@ export * from './Inbox';
 export * from './InvitesComponent';
 export * from './JoinRequestsComponent';
 export * from './NotificationComponent';
+export * from './NotificationDetails';
