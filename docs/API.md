b54f260c5aa0d3e57dd26a7578a1a77db96d8e88
kyoukaya <kyoukaya@gmail.com>
2019-11-22 14:29:13 +1100

    documentation

--- docs/API.md
+++ docs/API.md
@@ -327,7 +327,6 @@ to true, then the endpoint will return a list of pending incoming join
 requests to the projects that they own. 
 
 Example:
-```
 GET ->
 (200 OK) <-
 [
@@ -646,4 +645,97 @@ GET ->
             "avatar": "https://api.adorable.io/avatars/200/code_unity_default.png"
         }
     ]
+```
+
+### `/api/notify`
+#### GET
+Returns a list of notification objects for the current logged in user.
+There are several types of notifications, namely:
+- invite: when a user receives an invite to join a project
+- request: when a leader receives a request to join a project
+- join: sent to all members when a user joins a project
+- leave: sent to all members when a user leaves a project
+- kick: sent to all members when a user is kicked from a project
+- project_delete: sent to all members when a project is deleted
+
+All types of notifications have these fields:
+```
+{
+    "datetime": number,  # ms since epoch
+    "type": str,         # one of the types of notifications described above
+}
+```
+
+Additional `invite` fields:
+```
+{
+    "project_title": str,
+    "project_id": str
+}
+```
+
+Additional `request` fields:
+```
+{
+    "project_title":  str,
+    "project_id": str,
+    "requester": {
+        "username": str,
+        "id": str
+    }
+}
+```
+
+Additional `join` fields:
+```
+{
+    "project_title": str,
+    "project_id": str,
+    "joined_user": {
+        "username": str,
+        "id": str
+    }
+}
+```
+
+Additional `leave` fields:
+```
+{
+    "project_title": str,
+    "project_id": str,
+    "left_user": {
+        "username": str,
+        "id": str
+    }
+}
+```
+
+Additional `kick` fields:
+```
+{
+    "project_title": str,
+    "project_id": str,
+    "kicked_user": {
+        "username": str,
+        "id": str
+    }
+}
+```
+
+Additional `project_delete` fields:
+```
+{
+    "project_title": str
+}
+```
+
+#### DELETE
+Dismisses 1 notification, or all of them. Will return a 404 if the specified
+notification ID is not found.
+
+Expects:
+```
+{
+    "notification": str,  # either the notification id, or the string "all"
+}
 ```
--- server/resource/notification.py
+++ server/resource/notification.py
@@ -20,7 +20,85 @@ class NotificationResource(Resource):
     @jwt_required
     def get(self):
         """
-        Gets a list of all notifications for the current logged in user.
+        Returns a list of notification objects for the current logged in user.
+        There are several types of notifications, namely:
+        - invite: when a user receives an invite to join a project
+        - request: when a leader receives a request to join a project
+        - join: sent to all members when a user joins a project
+        - leave: sent to all members when a user leaves a project
+        - kick: sent to all members when a user is kicked from a project
+        - project_delete: sent to all members when a project is deleted
+    
+        All types of notifications have these fields:
+        ```
+        {
+            "datetime": number,  # ms since epoch
+            "type": str,         # one of the types of notifications described above
+        }
+        ```
+
+        Additional `invite` fields:
+        ```
+        {
+            "project_title": str,
+            "project_id": str
+        }
+        ```
+
+        Additional `request` fields:
+        ```
+        {
+            "project_title":  str,
+            "project_id": str,
+            "requester": {
+                "username": str,
+                "id": str
+            }
+        }
+        ```
+
+        Additional `join` fields:
+        ```
+        {
+            "project_title": str,
+            "project_id": str,
+            "joined_user": {
+                "username": str,
+                "id": str
+            }
+        }
+        ```
+
+        Additional `leave` fields:
+        ```
+        {
+            "project_title": str,
+            "project_id": str,
+            "left_user": {
+                "username": str,
+                "id": str
+            }
+        }
+        ```
+
+        Additional `kick` fields:
+        ```
+        {
+            "project_title": str,
+            "project_id": str,
+            "kicked_user": {
+                "username": str,
+                "id": str
+            }
+        }
+        ```
+
+        Additional `project_delete` fields:
+        ```
+        {
+            "project_title": str
+        }
+        ```
         """
         cur_user = cast(User, current_user)
 
@@ -35,6 +113,17 @@ class NotificationResource(Resource):
 
     @jwt_required
     def delete(self):
+        """
+        Dismisses 1 notification, or all of them. Will return a 404 if the specified
+        notification ID is not found.
+        
+        Expects:
+        ```
+        {
+            "notification": str,  # either the notification id, or the string "all"
+        }
+        ```
+        """
         parser = RequestParser()
         parser.add_argument("notification", required=True)
         args = parser.parse_args(strict=True)
