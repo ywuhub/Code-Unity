2acb6121bb81e1d690607cbca5ee7d06b08395ee
Zhiwei Cao <zhiwei.cao@student.unsw.edu.au>
2019-11-24 18:44:15 +1100

    change apiUrl back

--- frontend/code-unity/webpack.config.js
+++ frontend/code-unity/webpack.config.js
@@ -60,7 +60,7 @@ module.exports = {
   externals: {
     // global app config object
     config: JSON.stringify({
-      apiUrl: 'localhost:4000'
+      apiUrl: 'http://localhost:4000'
     })
   }
 }
